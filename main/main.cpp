ed86a2eb05fc17e47dcc48e62f2b863123b166be
Валерия Удалова <z1rfie@gmail.com>
2025-09-16 19:19:12 +0300

    change main

--- main/main.cpp
+++ main/main.cpp
@@ -1,37 +1,95 @@
-// Copyright 2024 Marina Usova
-
-#define EASY_EXAMPLE
-#ifdef EASY_EXAMPLE
-
 #include <iostream>
+
+#define CIRCLE
+#ifdef CIRCLE
+
 #include <iomanip>
-#include "../lib_easy_example/easy_example.h"
+#include "../lib_circle/circle.h"
 
-int main() {
-  int a, b;
-  float result;
+enum TypeTouch { ExternalTouch, Intersection, Coincidence, Inside, NotIntersect };
 
-  a = 1; b = 4;
+TypeTouch location_circles(Circle first_circle, Circle second_circle) {
+	Point first_circle_center = first_circle.get_center();
+	Point second_circle_center = second_circle.get_center();
+	float distance = first_circle_center.distance_between_points(second_circle_center);
 
-  try {
-      result = division(a, b);
-      std::cout << a << " / " << b << " = "
-          << std::setprecision(2) << result << std::endl;
-  } catch (std::exception err) {
-      std::cerr << err.what() << std::endl;
-  }
+	float radius_first_circle = first_circle.get_radius();
+	float radius_second_circle = second_circle.get_radius();
 
-  a = 1; b = 0;
-
-  try {
-      result = division(a, b);
-      std::cout << a << " / " << b << " = "
-          << std::setprecision(2) << result << std::endl;
-  } catch (std::exception err) {
-      std::cerr << err.what() << std::endl;
-  }
-
-  return 0;
+	if (distance > radius_first_circle + radius_second_circle) {
+		return NotIntersect;
+	}
+	else if (distance < radius_first_circle - radius_second_circle) {
+		return Inside;
+	}
+	else if (radius_first_circle - radius_second_circle < distance
+		&& distance < radius_first_circle + radius_second_circle) {
+		return Intersection;
+	}
+	else if (distance == radius_first_circle + radius_second_circle) {
+		return ExternalTouch;
+	}
+	else if (radius_first_circle == radius_second_circle && distance == 0) {
+		return Coincidence;
+	}
 }
 
-#endif  // EASY_EXAMPLE
+int main() {
+	Circle c1(Point(0, 0), 5);
+	Circle c2(Point(0, 0), 3);
+	std::cout << location_circles(c1, c2) << std::endl;
+
+	Circle c4(Point(1, 2), 3);
+	std::cout << location_circles(c1, c4) << std::endl;
+
+	Circle c6(Point(6, 0), 1);
+	std::cout << location_circles(c1, c6) << std::endl;
+
+	Circle c8(Point(0, 0), 5);
+	std::cout << location_circles(c1, c8) << std::endl;
+
+	Circle c10(Point(7, 9), 3);
+	std::cout << location_circles(c1, c10) << std::endl;
+
+	return 0;
+}
+
+#endif  // CIRCLE
+
+#ifdef MATRIX
+
+#include "matrix.h"
+
+int main() {
+	setlocale(LC_ALL, "rus");
+
+	size_t N1, M1;
+	std::cout << "Введите количество строк и столбцов для первой матрицы через пробел: ";
+	std::cin >> N1 >> M1;
+
+	size_t N2, M2;
+	std::cout << "Введите количество строк и столбцов для второй матрицы через пробел: ";
+	std::cin >> N2 >> M2;
+
+	Matrix<MathVector<size_t>> first_matrix(N1,M1);
+
+	char operation;
+	std::cout << "Выберите операцию (+, -, *): ";
+	std::cin >> operation;
+
+	switch (operation) {
+	case '+': 
+		std::cout << "to do +";
+		break;
+	case '-':
+		std::cout << "to do -";
+		break;
+	case '*':
+		std::cout << "to do *";
+		break;
+	}
+
+	return 0;
+}
+
+#endif // MATRIX
