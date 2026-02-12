a617416466d3949f011df566a22c4a032846e8f8
Валерия Удалова <z1rfie@gmail.com>
2026-02-12 18:40:37 +0300

    the matrix branch has been cleared

--- lib_easy_example/easy_example.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2024 Marina Usova
-
-#ifndef LIB_EASY_EXAMPLE_EASY_EXAMPLE_H_
-#define LIB_EASY_EXAMPLE_EASY_EXAMPLE_H_
-
-float division(int a, int b);
-
-#endif  // LIB_EASY_EXAMPLE_EASY_EXAMPLE_H_
--- lib_parser/parser.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdexcept>
-#include "../lib_easy_example/easy_example.h"
-
-float division(int a, int b) {
-    if (b == 0) {
-        throw std::invalid_argument("Input Error: can't divide by zero!");
-    }
-    return static_cast<float>(a) / b;
-}
